cafcc0329c43835cb410a55ace3b4fd6c6f234e8
VictorDPA <vicnoob@gmail.com>
2022-09-30 19:10:38 -0300

    task 10 completed

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const funcionario = data.employees.filter((pessoa) => (pessoa.id === id))
+    .map((el) => el.responsibleFor[0]).toString();
+  const animal = data.species.filter((bicho) =>
+    bicho.id === funcionario)
+    .map((el) => el.residents).flat(1)
+    .reduce((fim, inicio) =>
+      (fim.age > inicio.age ? fim : inicio));
+
+  return Object.values(animal);
 }
+console.log(getOldestFromFirstSpecies('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = getOldestFromFirstSpecies;
