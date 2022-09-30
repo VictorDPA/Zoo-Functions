df08c0536bfb9e691c935fc73445f1363cd7bbe5
VictorDPA <vicnoob@gmail.com>
2022-09-29 22:05:46 -0300

    task 4.1 completed

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
-}
+const local = data.employees;
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
-}
+const isManager = (id) =>
+  local.map((el) => el.managers)
+    .reduce((el1, el2) => el1.concat(el2))
+    .filter((item, pos, arr) => arr.indexOf(item) === pos)
+    .some((el) => el === id);
+
+console.log(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+
+const getRelatedEmployees = (managerId) => {
+  local.map((el) => el.managers)
+    .reduce((el1, el2) => el1.concat(el2))
+    .filter((item, pos, arr) => arr.indexOf(item) === pos);
+  return managerId;
+};
 
 module.exports = { isManager, getRelatedEmployees };
