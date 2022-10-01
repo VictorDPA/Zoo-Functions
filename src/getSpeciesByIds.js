4fa38fa45c9fb92e302649d759ccdf78c1bc3221
VictorDPA <vicnoob@gmail.com>
2022-09-30 22:05:16 -0300

    task 1 completed

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,22 +3,10 @@ const data = require('../data/zoo_data');
 const local = data.species;
 
 function getSpeciesByIds(...ids) {
+  if (!ids) return [];
   const check = ids;
-  console.log(check);
-  const mapear = local.reduce((final, pesquisa, i) =>
-    (final.filter((el) => el.id === check[i])
-    + pesquisa.filter((el) => el.id === check[i])), []);
+  const mapear = local.filter((el, _) => (el.id === check[_]) || (el.id === check[0]));
   return mapear;
 }
-// console.log(local);
-console.log(getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed', '0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
-
-// function getSpeciesByIds(...ids) {
-//   const check = { ids };
-//   console.log(check);
-//   const mapear = species.filter((animal, i) => (animal.id === check.ids ? local[i] : false))
-//     .map((mapa) => mapa);
-//   return mapear;
-// }
