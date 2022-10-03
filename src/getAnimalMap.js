f318fcd7bd3bf6b1ccc63c72499c0fffcdfc4b40
VictorDPA <vicnoob@gmail.com>
2022-10-03 12:36:09 -0300

    Seeing if evaluator will work

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,73 @@
+// const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// function bySexAndOrder(gender) {
+//   const pos = species.map((el) => el.location)
+//     .filter((el, ind, arr) => arr.indexOf(el) === ind)
+//     .reduce((fim, ini) => ({ ...fim,
+//       [ini]:
+//   species.filter((proj) => proj.location.includes(ini))
+//     .map((tipo) => ({ [tipo.name]: species.filter((res) =>
+//       res.name === tipo.name).map((k) => k.residents).flat(1)
+//       .filter((again) => again.sex === gender)
+//       .map((l) => l.name)
+//       .sort(),
+//     })),
+//     }), {});
+//   return pos;
+// }
+// function bySex(gender) {
+//   const pos = species.map((el) => el.location)
+//     .filter((el, ind, arr) => arr.indexOf(el) === ind)
+//     .reduce((fim, ini) => ({ ...fim,
+//       [ini]: species.filter((proj) => proj.location.includes(ini))
+//         .map((tipo) => ({ [tipo.name]: species.filter((res) => res.name === tipo.name)
+//           .map((k) => k.residents).flat(1).filter((again) => again.sex === gender)
+//           .map((l) => l.name),
+//         })),
+//     }), {});
+//   return pos.NE[1];
+// }
+// function fullMapWithNamesAndSorted() {
+//   const pos = species.map((el) => el.location)
+//     .filter((el, ind, arr) => arr.indexOf(el) === ind).reduce((fim, ini) => ({ ...fim,
+//       [ini]: species.filter((proj) => proj.location.includes(ini)).map((tipo) =>
+//         ({ [tipo.name]: species.filter((res) => res.name === tipo.name)
+//           .map((k) => k.residents).flat(1).map((l) => l.name)
+//           .sort() })),
+//     }), {});
+//   return pos;
+// }
+
+// function fullMapWithNames() {
+//   const pos = species.map((el) => el.location)
+//     .filter((el, ind, arr) => arr.indexOf(el) === ind).reduce((fim, ini) => ({ ...fim,
+//       [ini]: species.filter((proj) => proj.location.includes(ini))
+//         .map((tipo) => ({ [tipo.name]: species.filter((res) => res.name === tipo.name)
+//           .map((k) => k.residents).flat(1).map((l) => l.name) })),
+//     }), {});
+//   return pos.NE[0];
+// }
+// console.log(fullMapWithNames());
+// function fullMap() {
+//   const pos = data.species.map((el) => el.location)
+//     .filter((el, ind, arr) => arr.indexOf(el) === ind).reduce((a, b) => ({ ...a,
+//       [b]: species.filter((proj) => proj.location.includes(b))
+//         .map((loc) => loc.name),
+//     }), {});
+
+//   return pos;
+// }
+
 function getAnimalMap(options) {
-  // seu código aqui
+
 }
+// console.log((getAnimalMap({ sex: 'female' })));
 
 module.exports = getAnimalMap;
+
+// if (!options) return fullMap();
+//   const { includeNames, sex, sorted } = options;
+//   return sex === 'female' ? bySex('female') : bySex('male');
+
+//   return fullMapWithNames();
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -9,9 +9,9 @@ const isManager = (id) =>
     .includes(id); // includes cai muito melhor e mais rápido, neste caso.
 
 const getRelatedEmployees = (managerId) => {
-  const mapear = local.map((el) => el.managers)
-    .reduce((el1, el2) => el1.concat(el2))
-    .filter((item, pos, arr) => arr.indexOf(item) === pos);
+  // const mapear = local.map((el) => el.managers)
+  //   .reduce((el1, el2) => el1.concat(el2))
+  //   .filter((item, pos, arr) => arr.indexOf(item) === pos);
   const empregados = local.reduce((final, atual) => {
     if (atual.managers.includes(managerId)) {
       final.push(atual);
