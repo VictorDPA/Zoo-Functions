97e6907bc8cf24c1b35e5a9f947c3d0513ce7374
VictorDPA <vicnoob@gmail.com>
2022-10-04 17:41:23 -0300

    clear lint

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,5 +1,6 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
+const closed = 'The zoo is closed';
 describe('Testes da função getOpeningHours', () => {
   it('quando o parâmetro da função estiver vazio, retorna um objeto com todos os horários', () => {
     const actual = getOpeningHours();
@@ -16,8 +17,7 @@ describe('Testes da função getOpeningHours', () => {
   });
   it('quando chamado na segunda feira, espera-se que retorne o "The zoo is cloed"', () => {
     const actual = getOpeningHours('Monday', '09:00-AM');
-    const expected = 'The zoo is closed';
-    expect(actual).toEqual(expected);
+    expect(actual).toEqual(closed);
   });
   it('quando passado um valor de horas diferente de 0 até 12, retorna erro', () => {
     expect(() => getOpeningHours('Friday', '16:00-AM')).toThrow('The hour must be between 0 and 12');
@@ -36,17 +36,14 @@ describe('Testes da função getOpeningHours', () => {
   });
   it('quando chamado em qualquer outro dia da semana, dentro do horário de funcionamento, espera-se que retorne o "The zoo is cloed"', () => {
     const actual = getOpeningHours('Wednesday', '11:00-AM');
-    const expected = 'The zoo is open';
-    expect(actual).toEqual(expected);
+    expect(actual).toEqual(closed);
   });
   it('quando chamado em qualquer outro dia da semana, fora do horário de funcionamento, espera-se que retorne o "The zoo is cloed"', () => {
     const actual = getOpeningHours('Wednesday', '12:00-AM');
-    const expected = 'The zoo is closed';
-    expect(actual).toEqual(expected);
+    expect(actual).toEqual(closed);
   });
   it('quando chamado em qualquer outro dia da semana, dentro do horário de fechamento, na parte da tarde, espera-se que retorne o "The zoo is cloed"', () => {
     const actual = getOpeningHours('Wednesday', '05:00-PM');
-    const expected = 'The zoo is closed';
-    expect(actual).toEqual(expected);
+    expect(actual).toEqual(closed);
   });
 });
